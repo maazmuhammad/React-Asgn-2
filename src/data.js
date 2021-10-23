a77397eb78a856325723d2a66a65467bc17c5426
maazmuhammad <maazmuhammad88@gmail.com>
2021-10-23 17:15:03 +0500

    First commit

---
+++ src/data.js
@@ -0,0 +1,49 @@
+let data = [
+    {
+        name: "Checkered Button Down Shirt with Chinese Collar",
+        price: 990,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/TBTF313007REDWHT.jpg?v=1626522612",
+    },
+    {
+        name: "Checkered Button Down Shirt",
+        price: 1190,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/TBTF313005REDORG_2.jpg?v=1626156133",
+    },
+    {
+        name: "Checkered Button Down Shirt",
+        price: 1690,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/F0011303618.jpg?v=1632301919",
+    },
+    {
+        name: "Washed Out Ripped Denim Jeans",
+        price: 1999.00,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/F0010509902_3.jpg?v=1634894299",
+    },
+    {
+        name: "Basic Studio Shirt",
+        price: 2499.00,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/F0003303001.jpg?v=1632300969",
+    },
+    {
+        name: "Checkered Button Down Shirt",
+        price:2199.00,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/F0002308902.jpg?v=1634550002",
+    },
+    {
+        name: "Basic Button Down Shirt",
+        price: 2299.00,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/TBTF313006WHT_1.jpg?v=1629869723",
+    },
+    {
+        name: "Checkered Button Down Shirt",
+        price: 2199.00,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/F0013303618.jpg?v=1632293061",
+    },
+    {
+        name: "Textured Button Down Shirt",
+        price: 2199.00,
+        img1: "https://cdn.shopify.com/s/files/1/2290/7887/products/TBTF313003CLDPNK_3.jpg?v=1626086312",
+    }
+]
+
+export {data}
